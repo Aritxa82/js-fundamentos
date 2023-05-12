2289de5830eee519626b776c88e1a73497ee939a
Aritxa Troyano <aritxa.troyano@live.com>
2023-05-12 20:32:14 +0200

    intento

---
+++ 01-variables.js
@@ -0,0 +1,33 @@
+
+/*
+FINAL_TEXT  = PASCAL_CASE
+FinalText = CAMEL_CASE 
+*/
+
+
+var greetings = 'hola desde';
+var school = 'pmba';
+
+var greetingsFromSchool = greetings + ' ' + school;
+
+console.log(greetingsFromSchool);
+
+
+/*
+string = cadena de caracteres
+number = numeros
+boolean = true o false
+array
+object
+*/
+
+var ageAsNumber = 30;
+var ageAsString = '30';
+
+
+var arrayOfValues = [30, '30', true, false, 4];
+
+var objectOfValues = {
+  age: 30,
+  ageAsNumber: ageAsNumber
+};
